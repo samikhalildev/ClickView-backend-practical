ee78b78ad1e51c67aaf162453c23c6e3409175d5
samikhalildev <sami.khalil.dev@gmail.com>
2019-01-10 14:24:31 +1100

    API routes completed

--- routes/api/videoAPI.js
+++ routes/api/videoAPI.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 // Load Models
 const Video = require('../../models/Video');
 
+
+
 /*  @route      GET api/videos
     @desc       View all videos
     @access     Public
@@ -16,49 +18,63 @@ router.get('/', (req, res) => {
 });
 
 
+
+
+
 /*  @route      POST api/videos
-    @desc       Create a video
+    @desc       Create a video or update a video
     @access     Public
  */
 router.post('/', (req, res) => {
 
-    var name = req.body.name;
-    var duration = Number(req.body.duration);
-    var description = req.body.description;
-    var thumbnail = req.body.thumbnail;
-    var folder = req.body.folder;
-    var tags = req.body.tags;
+    // 1: Get fields
+    const newVideo = {};
 
-    // Validation
-    req.checkBody("name", "Name is required").notEmpty();
-    req.checkBody("duration", "Duration is required").notEmpty();
-    req.checkBody("description", "Description is required").notEmpty();
-    req.checkBody("thumbnail", "thumbnail is required").notEmpty();
-    req.checkBody("folder", "folder is required").notEmpty();
-    req.checkBody("tags", "tags are required").notEmpty();
+    // Making sure to only get fields that are sent in the body request.
+    if(req.body.name) newVideo.name = req.body.name;
+    if(req.body.duration) newVideo.duration = Number(req.body.duration);
+    if(req.body.description) newVideo.description = req.body.description;
+    if(req.body.thumbnail) newVideo.thumbnail = req.body.thumbnail;
+    if(req.body.folder) newVideo.folder = req.body.folder;
+    if(req.body.tags) newVideo.tags = req.body.tags;
 
-    var errors = req.validationErrors();
+    // 2: Check if the video name already exists.
+    //   - If video exists then update the fields in the body of the request
+    //   - Otherwise create a new video.
+    Video.findOne({ name: newVideo.name })
+        .then(video => {
 
-    if(errors){
-        res.status(400).json(errors);
+            if(video) {
+                Video.findOneAndUpdate({ name: newVideo.name }, { $set: newVideo }, { new: true })
+                    .then(videoUpdated => res.json(videoUpdated))
+                    .catch(err => res.status(400).json(err));
 
-    } else {
-        // Create a new video object
-        const newVideo = new Video ({
-            name,
-            duration,
-            description,
-            thumbnail,
-            folder,
-            tags
-        });
+            } else {
+                new Video(newVideo)
+                    .save()
+                    .then(videoSaved => res.json(videoSaved))
+                    .catch(err => res.status(400).json(err));
+            }
 
-        newVideo.save()
-            .then(post => res.json(post))
-            .catch(err => res.status(400).json(err));
-    }
+        })
+        .catch(err => console.log(err));
 
 });
 
+
+
+/*  @route      DELETE api/videos/:video_id
+    @desc       Delete video by id
+    @access     Public
+ */
+router.delete("/:video_id", (req, res) => {
+
+    const videoID = req.params.video_id;
+
+    Video.remove({ _id: videoID })
+        .then(done => res.json({ success: 'Video has been removed' }))
+        .catch(err => res.status(404).json({ error: 'Video not found' }));
+
+});
 
 module.exports = router;
